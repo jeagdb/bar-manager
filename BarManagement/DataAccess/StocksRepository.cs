17a05f996a599cbbc6984c171beebd1d58b7a933
Feng Chung <feng.chung@epita.fr>
2020-04-24 02:24:45 +0200

    repository(add): for Cocktails, Drinks, Stocks and Transactions, need to test

---
+++ BarManagement/DataAccess/StocksRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BarManagement.DataAccess.EfModels;
+
+namespace BarManagement.DataAccess
+{
+    public class StocksRepository : Repository<Stocks, Models.Stocks>
+    {
+        public StocksRepository(barDBContext context, IMapper mapper)
+            : base(context, mapper)
+        {
+        }
+    }
+}
---
+++ BarManagement/DataAccess/TransactionsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BarManagement.DataAccess.EfModels;
+
+namespace BarManagement.DataAccess
+{
+    public class TransactionsRepository : Repository<Transactions, Models.Transactions>
+    {
+        public TransactionsRepository(barDBContext context, IMapper mapper)
+            : base(context, mapper)
+        {
+        }
+    }
+}
