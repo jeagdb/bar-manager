f20127f51161e3d965ea16b9b46cada80dbc21ee
Feng Chung <feng.chung@epita.fr>
2020-05-06 02:38:03 +0200

    [STATS PAGE] - begin display list of cocktails in table

--- BarManagement/Pages/Stats.cshtml.cs
+++ BarManagement/Pages/Stats.cshtml.cs
@@ -9,9 +9,39 @@ namespace BarManagement.Pages
 {
     public class StatsModel : PageModel
     {
-        public void OnGet()
+        private readonly DataAccess.Interfaces.ICocktailsRepository _cocktailsRepository;
+        private readonly DataAccess.Interfaces.ITransactionsRepository _transactionsRepository;
+
+        public List<Models.Cocktails> Cocktails { get; set; }
+        public List<Models.Transactions> Transactions { get; set; }
+        public List<int> earningsByCocktails { get; set; } // Bénéfices
+        public List<int> profitabilityByCocktails { get; set; } // Rentabilités
+
+        public StatsModel(DataAccess.Interfaces.ICocktailsRepository cocktailsRepository, DataAccess.Interfaces.ITransactionsRepository transactionRepository)
+        {
+            _cocktailsRepository = cocktailsRepository;
+            _transactionsRepository = transactionRepository;
+
+            earningsByCocktails = new List<int>();
+            profitabilityByCocktails = new List<int>();
+        }
+
+        private void CalculateEachEarnings()
+        {
+            
+        }
+
+        private void CalculateEachProfitability()
         {
 
+        }
+
+        public void OnGetAsync()
+        {
+            Cocktails = _cocktailsRepository.GetCocktails();
+            Transactions = _transactionsRepository.GetTransactions();
+            CalculateEachEarnings();
+            CalculateEachProfitability();
         }
     }
 }
