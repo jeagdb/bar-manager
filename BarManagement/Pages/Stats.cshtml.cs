f9ea4b684175dac68578ce7a359eb2168e38f037
Feng Chung <feng.chung@epita.fr>
2020-05-07 02:26:25 +0200

    [STATS] - calculate and display stats finished

--- BarManagement/Pages/Stats.cshtml.cs
+++ BarManagement/Pages/Stats.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BarManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -14,34 +15,39 @@ namespace BarManagement.Pages
 
         public List<Models.Cocktails> Cocktails { get; set; }
         public List<Models.Transactions> Transactions { get; set; }
-        public List<int> earningsByCocktails { get; set; } // Bénéfices
-        public List<int> profitabilityByCocktails { get; set; } // Rentabilités
 
         public StatsModel(DataAccess.Interfaces.ICocktailsRepository cocktailsRepository, DataAccess.Interfaces.ITransactionsRepository transactionRepository)
         {
             _cocktailsRepository = cocktailsRepository;
             _transactionsRepository = transactionRepository;
-
-            earningsByCocktails = new List<int>();
-            profitabilityByCocktails = new List<int>();
-        }
-
-        private void CalculateEachEarnings()
-        {
-            
-        }
-
-        private void CalculateEachProfitability()
-        {
-
         }
 
         public void OnGetAsync()
         {
             Cocktails = _cocktailsRepository.GetCocktails();
             Transactions = _transactionsRepository.GetTransactions();
-            CalculateEachEarnings();
-            CalculateEachProfitability();
+        }
+
+        public double[] CurrentCocktailStats(int id)
+        {
+            double nbSoldGlasses = 0;
+            double earning = 0;
+            double totalEarnings = 0;
+            double profitability = 0;
+
+            double priceToSell = Cocktails[id].PriceToSell;
+            double cost = Cocktails[id].Cost;
+
+            List<Transactions> cocktailTransactions = _transactionsRepository.GetTransactionsByCocktail(Cocktails[id].Id);
+            cocktailTransactions.ForEach(t => {
+                double div = t.Value / priceToSell;
+                nbSoldGlasses += div;
+            });
+
+            earning = priceToSell - cost;
+            totalEarnings = earning * nbSoldGlasses;
+            profitability = priceToSell / cost;
+            return new double[]{ nbSoldGlasses, earning, totalEarnings, profitability };
         }
     }
 }
