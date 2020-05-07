f152069bb77be14addb5b2d6c3101282522fce79
Thomas Franel <thomas.franel@epita.fr>
2020-05-07 23:05:20 +0200

    [COCKTAILS] - added the computation of costs for cocktails

--- BarManagement/Pages/Cocktails.cshtml.cs
+++ BarManagement/Pages/Cocktails.cshtml.cs
@@ -35,19 +35,23 @@ namespace BarManagement.Pages
         private readonly DataAccess.Interfaces.IDrinksRepository _drinksRepository;
         private readonly DataAccess.Interfaces.ICocktailsRepository _cocktailsRepository;
         private readonly DataAccess.Interfaces.ICocktailsCompositionRepository _cocktailsCompositionRepository;
+        private readonly DataAccess.Interfaces.IStocksRepository _stocksRepository;
 
         public List<Models.Cocktails> Cocktails { get; set; }
         public List<Models.Drinks> Drinks { get; set; }
         public List<SelectListItem> DrinksOptions { get; set; }
         [BindProperty]
         public List<Models.CocktailsComposition> Compositions { get; set; }
+        public List<Models.Stocks> Stocks { get; set; }
         public CocktailsModel(DataAccess.Interfaces.ICocktailsRepository cocktailsRepository,
             DataAccess.Interfaces.IDrinksRepository drinksRepository,
-            DataAccess.Interfaces.ICocktailsCompositionRepository cocktailsCompositionRepository)
+            DataAccess.Interfaces.ICocktailsCompositionRepository cocktailsCompositionRepository,
+            DataAccess.Interfaces.IStocksRepository stocksRepository)
         {
             _cocktailsRepository = cocktailsRepository;
             _drinksRepository = drinksRepository;
             _cocktailsCompositionRepository = cocktailsCompositionRepository;
+            _stocksRepository = stocksRepository;
             Compositions = new List<Models.CocktailsComposition>();
         }
 
@@ -68,11 +72,13 @@ namespace BarManagement.Pages
         {
             Drinks = _drinksRepository.GetDrinks();
             Cocktails = _cocktailsRepository.GetCocktails();
+            Stocks = _stocksRepository.GetStocks();
         }
         public async Task<IActionResult> OnPostCocktails()
         {
             ModelState.MarkAllFieldsAsSkipped();
             Drinks = _drinksRepository.GetDrinks();
+            Stocks = _stocksRepository.GetStocks();
             if (!TryValidateModel(FormCocktail, nameof(FormCocktail)))
             {
                 return Page();
@@ -80,10 +86,13 @@ namespace BarManagement.Pages
             Models.Cocktails newCocktail = await _cocktailsRepository.Insert(new Models.Cocktails() { Name = FormCocktail.NAME, PriceToSell = Double.Parse(FormCocktail.PRICE) });
             int index = 0;
             bool containAlcool = false;
+            double cost = 0;
             foreach (Models.CocktailsComposition composition in Compositions)
             {
                 var drinkId = Int32.Parse(Request.Form["drinkSelected_" + index ]);
                 Models.Drinks drink = Drinks.First(drink => drink.Id == drinkId);
+                Models.Stocks stocks = Stocks.First(stock => stock.DrinkId == drinkId);
+                cost += (Double) stocks.Price * composition.Quantity;
                 if (drink.Category == "Alcool") { containAlcool = true;  }
                 await _cocktailsCompositionRepository.Insert(new Models.CocktailsComposition() { DrinkId = drink.Id, CocktailId = newCocktail.Id, Quantity = composition.Quantity });
                 index++;
@@ -103,6 +112,7 @@ namespace BarManagement.Pages
             {
                 newCocktail.CocktailCategory = "Soft";
             }
+            newCocktail.Cost = cost;
             await _cocktailsRepository.Update(newCocktail);
             return Redirect("./Cocktails");
         }
