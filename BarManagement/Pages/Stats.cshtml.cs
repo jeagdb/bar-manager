f5edf9b02c3806c6240883b4892d1aeb2ca385ff
Feng Chung <feng.chung@epita.fr>
2020-05-08 14:07:02 +0200

    [STATS] - need to save this implementation of displaying in green for MaxNbGlassses but doesn't work yet

--- BarManagement/Pages/Stats.cshtml.cs
+++ BarManagement/Pages/Stats.cshtml.cs
@@ -16,6 +16,7 @@ namespace BarManagement.Pages
         public List<Models.Cocktails> Cocktails { get; set; }
         public List<Models.Transactions> Transactions { get; set; }
         public List<List<Models.Cocktails>> CocktailsSortedByCategory { get; set; }
+        public List<List<double>> NbSoldGlassesSortedByCategory { get; set; }
 
         public StatsModel(DataAccess.Interfaces.ICocktailsRepository cocktailsRepository, DataAccess.Interfaces.ITransactionsRepository transactionRepository)
         {
@@ -23,32 +24,48 @@ namespace BarManagement.Pages
             _transactionsRepository = transactionRepository;
         }
 
+        // Retourne une liste de nombre de verres dont la position dans la liste correspond
+        // à la position du cocktail dans la liste CocktailsSortedByCategory
+        public List<List<double>> CalculateNbGlassesForEachCocktail()
+        {
+            List<List<double>> nbGlassesList = new List<List<double>>();
+            foreach (var cocktailsByCategory in CocktailsSortedByCategory)
+            {
+                List<double> nbGlassesListByCategory = new List<double>();
+                foreach (var cocktail in cocktailsByCategory)
+                {
+                    double nbSoldGlasses = 0;
+                    List<Transactions> cocktailTransactions = _transactionsRepository.GetTransactionsByCocktail(cocktail.Id);
+                    cocktailTransactions.ForEach(t => {
+                        double div = t.Value / cocktail.PriceToSell;
+                        nbSoldGlasses += div;
+                    });
+                    nbGlassesListByCategory.Add(nbSoldGlasses);
+                }
+                nbGlassesList.Add(nbGlassesListByCategory);
+            }
+            return nbGlassesList;
+        }
+
         public void OnGetAsync()
         {
             Cocktails = _cocktailsRepository.GetCocktails();
             Transactions = _transactionsRepository.GetTransactions();
             CocktailsSortedByCategory = _cocktailsRepository.GetCocktailsSortedByCategory();
+            NbSoldGlassesSortedByCategory = CalculateNbGlassesForEachCocktail();
         }
 
-        public double[] CurrentCocktailStats(int id)
+        public double[] CurrentCocktailStats(int indexI, int indexJ)
         {
-            double nbSoldGlasses = 0;
-            double earning = 0;
-            double totalEarnings = 0;
-            double profitability = 0;
+            Cocktails currentCocktails = CocktailsSortedByCategory[indexI][indexJ];
 
-            double priceToSell = Cocktails[id].PriceToSell;
-            double cost = Cocktails[id].Cost;
+            double priceToSell = currentCocktails.PriceToSell;
+            double cost = currentCocktails.Cost;
 
-            List<Transactions> cocktailTransactions = _transactionsRepository.GetTransactionsByCocktail(Cocktails[id].Id);
-            cocktailTransactions.ForEach(t => {
-                double div = t.Value / priceToSell;
-                nbSoldGlasses += div;
-            });
-
-            earning = priceToSell - cost;
-            totalEarnings = earning * nbSoldGlasses;
-            profitability = priceToSell / cost;
+            double nbSoldGlasses = NbSoldGlassesSortedByCategory[indexI][indexJ];
+            double earning = priceToSell - cost;
+            double totalEarnings = earning * nbSoldGlasses;
+            double profitability = priceToSell / cost;
             return new double[]{ nbSoldGlasses, earning, totalEarnings, profitability };
         }
 
@@ -71,6 +88,46 @@ namespace BarManagement.Pages
                 default:
                     return "";
             }
+        }
+
+        public (double, double) FindMaxNbSoldGlassesByCategory(int indexI)
+        {
+            List<Cocktails> cocktails = CocktailsSortedByCategory[indexI];
+            List<double> nbSoldGlassesList = NbSoldGlassesSortedByCategory[indexI];
+
+            if (nbSoldGlassesList.Count == 1)
+            {
+                if (nbSoldGlassesList[0] == 0)
+                    return (-1, -1);
+                return (nbSoldGlassesList[0], -1);
+            }
+
+            if (nbSoldGlassesList.Count == 2)
+            {
+                if (nbSoldGlassesList[1] != 0)
+                    return (nbSoldGlassesList[0], nbSoldGlassesList[1]);
+                return (nbSoldGlassesList[0], -1);
+            }
+
+            double maxNbSold = nbSoldGlassesList[0];
+            double secondNbSold = nbSoldGlassesList[1];
+            for (int i = 1; i < nbSoldGlassesList.Count; i++)
+            {
+                if (nbSoldGlassesList[i] > maxNbSold)
+                {
+                    secondNbSold = maxNbSold;
+                    maxNbSold = nbSoldGlassesList[i];
+                }
+                else if (nbSoldGlassesList[i] > secondNbSold)
+                    secondNbSold = nbSoldGlassesList[i];
+            }
+
+            return (maxNbSold, secondNbSold);
+        }
+
+        public void FindSmallestNbGlassesSoldByCategory(int indexI)
+        {
+
         }
     }
 }
